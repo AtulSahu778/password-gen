4d985e8e50c418aee2b22779c6e6acbfb81e17a2
AtulSahu778 <sahuatul2005@gmail.com>
2025-05-29 23:37:41 +0530

    added more combinations in the password

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,20 @@ function App() {
   const passwordGen = useCallback(() => {
       let pass = ""
       let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-      if(numAllow) str += "0123456789"
-      if(charAllow) str += "!@#$%*+-_"
-
-
+      
+      if(numAllow) {
+        str += "0123456789"
+        str += "!@#$%^&*()_+-=[]{}|;:,.<>?/~`"
+        str += "€£¥¢§©®™°±×÷≠≈≤≥∞∑∏√∫∂∆∇"
+        str += "αβγδεζηθικλμνξοπρστυφχψω"
+        str += "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+      }
+      
+      if(charAllow) {
+        str += "!@#$%*+-_"
+        str += "()[]{}<>|\\/^&*+=~`"
+        str += "€£¥¢§©®™°±×÷≠≈≤≥∞∑∏√∫∂∆∇"
+      }
 
       for(let i=1; i< length; i++){
         let char = Math.floor(Math.random() * str.length)
